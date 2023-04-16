76592bdd52fee61d3f5b547e0c7c4a665b0f8050
Joel Willick <willickjb@gmail.com>
2023-04-16 12:10:59 -0400

    Fixed timer platforms

--- Assets/Scripts/TimerPlatforms.cs
+++ Assets/Scripts/TimerPlatforms.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TimerPlatform : MonoBehaviour
+public class TimerPlatforms : MonoBehaviour
 {
 
     public int platformType;
